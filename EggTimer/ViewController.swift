342412394739fdeeda15453f7eb71a50528c5cf0
lukaszslaweta <lukasz.slaweta@gmail.com>
2022-02-17 07:00:48 +0100

    Initial Commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Łukasz Sławęta on 17/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
